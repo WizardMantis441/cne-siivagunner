4bbdee1c7e8d4c239438b31072ad7e5b0df09e64
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2022-12-17 02:49:49 -0600

    a

--- data/global.hx
+++ data/global.hx
@@ -1,40 +1,63 @@
-return;
+// main week hud, botplay toggle, notesplashes on newgrounds builds
 
-import funkin.menus.FreeplayState;
-function updatePost(elapsed) {
-    if (Std.isOfType(FlxG.state, FreeplayState)) { // is in freeplay
-        // for (i in FlxG.state.iconArray.members) i.x = 0;
-        // for (i in FlxG.state.grpSongs.members) i.x = 10;
+import funkin.game.PlayState;
 
-        for (i in FlxG.state.grpSongs.members) {
-            // i.setGraphicSize(FlxG.width / 2, FlxG.height / 5);
-            i.screenCenter();
+var botplay:Bool = true;
+var vanillaHud:Bool = true;
+
+function postUpdate(elapsed) {
+    if (PlayState.instance != null) { // is in game
+        if (botplay) {
+            PlayState.instance.notes.forEach(function(n) {
+                if (n.strumTime <= Conductor.songPosition)
+                    PlayState.instance.goodNoteHit(n);
+            });
+
+            /*
+            PlayState.instance.playerStrums.forEach(function(p) {
+                p.animation.finishCallback = function(a) {
+                    if (a == 'confirm')
+                        p.playAnim('static');
+                }
+            });
+            */
         }
+
+        for (i in [PlayState.instance.accuracyTxt, PlayState.instance.missesTxt])
+            i.visible = (vanillaHud) ? false : true;
     }
 }
 
-import flixel.FlxG;
-import funkin.system.Conductor;
-import funkin.ui.Alphabet;
+function onPlayerHit(event:NoteHitEvent) {
+    trace('test');
 
-static var wavyAlphabet = true;
-var time:Float = 0;
+    var notesplash = new FlxSprite();
+    notesplash.frames = Paths.getSparrowAtlas('noteSplashes');
+    notesplash.cameras = [camHUD];
 
-function update(elapsed) {
-    if (!wavyAlphabet) return;
-    
-    time += elapsed;
-    var curState = FlxG.state;
-    while(curState != null) {
-        curState.forEach(function(spr) {
-            if (Std.isOfType(spr, Alphabet)) {
-                var i = 0;
-                spr.forEach(function(letter) {
-                    letter.y = spr.y + (Math.sin((time + (i / 4)) * Math.PI) * 10);
-                    i++;
-                });
-            }
-        }, true);
-        curState = curState.subState;
+    notesplash.animation.addByPrefix('note1-0', 'note impact 1  blue', 24, false);
+    notesplash.animation.addByPrefix('note2-0', 'note impact 1 green', 24, false);
+    notesplash.animation.addByPrefix('note0-0', 'note impact 1 purple', 24, false);
+    notesplash.animation.addByPrefix('note3-0', 'note impact 1 red', 24, false);
+    notesplash.animation.addByPrefix('note1-1', 'note impact 2 blue', 24, false);
+    notesplash.animation.addByPrefix('note2-1', 'note impact 2 green', 24, false);
+    notesplash.animation.addByPrefix('note0-1', 'note impact 2 purple', 24, false);
+    notesplash.animation.addByPrefix('note3-1', 'note impact 2 red', 24, false);
+
+    notesplash.setPosition(event.note.x, event.note.y);
+    notesplash.alpha = 0.6;
+    notesplash.animation.play('note' + event.note.noteData + '-' + FlxG.random.int(0, 1), true);
+    notesplash.animation.finishCallback = function(a) {
+        notesplash.remove();
     }
+    add(notesplash);
+
+    notesplash.animation.curAnim.frameRate += FlxG.random.int(-2, 2);
+    notesplash.updateHitbox();
+    notesplash.offset.set(width * 0.3, height * 0.3);
 }
+
+/*
+function onKeyShit(event:InputSystemEvent)
+    if (botplay) event.cancel();
+*/
